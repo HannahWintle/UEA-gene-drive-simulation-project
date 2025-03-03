1077427f8e8c3f196c4357abee9890a093007481
HannahWintle <cnm20syu@uea.ac.uk>
2025-03-03 12:53:02 +0000

    Updated test script to include different parameters for males and females.

--- scripts/test_mgdrive.R
+++ scripts/test_mgdrive.R
@@ -1,58 +1,64 @@
-## testing mgdrive====
+## Testing MEREA in MGDrivE
 ####################
 # Load libraries
 ####################
 library(MGDrivE)
-#> Loading MGDrivE: Mosquito Gene Drive Explorer
 
 ####################
 # Output Folder
 ####################
 dir.create("mgdrive")
-outFolder <- "mgdrive/run005"
+outFolder <- "mgdrive/run_MEREA_003"
 
 ####################
 # Simulation Parameters
 ####################
-# days to run the simulation
-tMax <- 365
+tMax <- 365  # Simulation run time
 
-# entomological parameters
+# Entomological parameters (unchanged)
 bioParameters <- list(betaK=20, tEgg=5, tLarva=6, tPupa=4, popGrowth=1.175, muAd=0.09)
 
-# a 1-node network where mosquitoes do not leave
+# Single-node population model (no migration)
 moveMat <- matrix(data = 1, nrow = 1, ncol = 1)
 
-# parameters of the population equilibrium
+# Population equilibrium size
 adultPopEquilibrium <- 500
 sitesNumber <- nrow(moveMat)
 
 ####################
 # Basic Inheritance pattern
 ####################
-# Mendelian cube with standard (default) parameters
-cube <- cubeMEDEA()
-
+# Using the MEREA cube instead of MEDEA
+cube <- cubeMEREA()
 
 ####################
 # Setup releases and batch migration
 ####################
 
+# Initialize release settings
 patchReleases <- replicate(n=sitesNumber,
-                           expr={list(maleReleases=NULL,femaleReleases=NULL,
-                                      eggReleases=NULL,matedFemaleReleases=NULL)},
+                           expr={list(maleReleases=NULL, femaleReleases=NULL,
+                                      eggReleases=NULL, matedFemaleReleases=NULL)},
                            simplify=FALSE)
 
-releasesParameters <- list(releasesStart=0,  # Release starts at generation 0
-                           releasesNumber=1,  # Single release event
-                           releasesInterval=0,
-                           releaseProportion=50)
+# Define separate release proportions for male and female
+releasesParametersMale <- list(releasesStart=0, # Release starts at generation 0
+                               releasesNumber=1, # Single release event 
+                               releasesInterval=0,  
+                               releaseProportion=200) #Can tweak male and female releases separately #Adjust threshold testing here
 
-releasesVector <- generateReleaseVector(driveCube=cube,
-                                        releasesParameters=releasesParameters)
+releasesParametersFemale <- list(releasesStart=0,  
+                                 releasesNumber=1,  
+                                 releasesInterval=0,  
+                                 releaseProportion=200)  # female release
 
-patchReleases[[1]]$maleReleases <- releasesVector
-patchReleases[[1]]$femaleReleases <- releasesVector
+# Generate release vectors for both sexes
+maleReleasesVector <- generateReleaseVector(driveCube=cube, releasesParameters=releasesParametersMale)
+femaleReleasesVector <- generateReleaseVector(driveCube=cube, releasesParameters=releasesParametersFemale)
+
+# Assign release vectors to appropriate sex
+patchReleases[[1]]$maleReleases <- maleReleasesVector
+patchReleases[[1]]$femaleReleases <- femaleReleasesVector
 
 # batch migration is disabled by setting the probability to 0
 # This is required because of the stochastic simulations, but doesn't make sense
@@ -60,23 +66,20 @@ patchReleases[[1]]$femaleReleases <- releasesVector
 batchMigration <- basicBatchMigration(batchProbs=0,
                                       sexProbs=c(.5,.5), numPatches=sitesNumber)
 
-
-
 ####################
 # Combine parameters and run!
 ####################
-# set MGDrivE to run deterministic
+# Set MGDrivE to deterministic mode
 setupMGDrivE(stochasticityON = FALSE, verbose = FALSE)
 
-# setup parameters for the network. This builds a list of parameters required for
-#  every population in the network. In ths case, we havee a network of 1 population.
-netPar <- parameterizeMGDrivE(runID=1, simTime=tMax, sampTime = 1, nPatch=sitesNumber,
+# Define network parameters
+netPar <- parameterizeMGDrivE(runID=1, simTime=tMax, sampTime=1, nPatch=sitesNumber,
                               beta=bioParameters$betaK, muAd=bioParameters$muAd,
                               popGrowth=bioParameters$popGrowth, tEgg=bioParameters$tEgg,
                               tLarva=bioParameters$tLarva, tPupa=bioParameters$tPupa,
-                              AdPopEQ=adultPopEquilibrium, inheritanceCube = cube)
+                              AdPopEQ=adultPopEquilibrium, inheritanceCube=cube)
 
-# build network prior to run
+# Initialize simulation network
 MGDrivESim <- Network$new(params=netPar,
                           driveCube=cube,
                           patchReleases=patchReleases,
@@ -86,8 +89,8 @@ MGDrivESim <- Network$new(params=netPar,
                           directory=outFolder,
                           verbose=FALSE)
 
-# run simulation
-MGDrivESim$oneRun(verbose = T)
+# Run simulation
+MGDrivESim$oneRun(verbose = TRUE)
 
 ####################
 # Post Analysis
@@ -102,4 +105,4 @@ aggregateFemales(readDir = outFolder, genotypes = cube$genotypesID,
                  remFile = TRUE, verbose = FALSE)
 
 # plot output to see effect
-plotMGDrivESingle(readDir = outFolder, totalPop = TRUE, lwd = 3.5, alpha = 1)
+plotMGDrivESingle(readDir = outFolder, totalPop = TRUE, lwd = 3.5, alpha = 1)              

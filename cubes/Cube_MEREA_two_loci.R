fcdf42dee1c55a78dadf7acd48d788b420f10f0b
HannahWintle <cnm20syu@uea.ac.uk>
2025-03-19 20:42:53 +0000

    all probabilities in tMatrix should be accurate and correct now

--- cubes/Cube_MEREA_two_loci.R
+++ cubes/Cube_MEREA_two_loci.R
@@ -77,47 +77,47 @@ cubeMEREA_2L <- function(rM = 0, Teff = 1.0, eta = NULL, phi = NULL,
   tMatrix['ZW', 'ZZ', c('ZW', 'ZZ')] <- c(1, 1) / 2  # Wild-type cross
   
   tMatrix['ZW', 'MaZ', c('MaZ', 'ZZ', 'MaW', 'ZW', 'RZ', 'RW')] <- c((1 - rM), 1, (1 - rM), 1, rM, rM) / 4 # checked
-  tMatrix['ZW', 'MbZ', c('MbZ', 'ZZ', 'MbW', 'ZW', 'RZ', 'RW')] <- c((1 - rM), 1, (1 - rM), 1, rM, rM) / 4 
-  tMatrix['ZW', 'MaMa', c('MaZ', 'MaW', 'RZ', 'RW')] <- c((1 - rM), (1 - rM), rM, rM) / 2
-  tMatrix['ZW', 'MbMb', c('MbZ', 'MbW', 'RZ', 'RW')] <- c((1 - rM), (1 - rM), rM, rM) / 2
-  tMatrix['ZW', 'MaMb', c('MaZ', 'MbZ', 'MaW', 'MbW', 'RZ', 'RW')] <- c((1 - rM), (1 - rM), (1 - rM), (1 - rM), rM, rM) / 4
-  tMatrix['ZW', 'RZ', c('ZZ', 'RZ', 'ZW', 'RW')] <- c(1, 1, 1, 1) / 4
-  tMatrix['ZW', 'MaR', c('MaZ', 'RZ', 'MaW', 'RW')] <- c(1-rM, (1-rM)+rM*2, 1-rM, (1-rM)+rM*2)/4
-  tMatrix['ZW', 'MbR', c('MbZ', 'RZ', 'MbW', 'RW')] <- c(1-rM, (1-rM)+rM*2, 1-rM, (1-rM)+rM*2)/4
-  tMatrix['ZW', 'RR', c('RZ', 'RW')] <- c(1, 1) / 2
+  tMatrix['ZW', 'MbZ', c('MbZ', 'ZZ', 'MbW', 'ZW', 'RZ', 'RW')] <- c((1 - rM), 1, (1 - rM), 1, rM, rM) / 4 # checked
+  tMatrix['ZW', 'MaMa', c('MaZ', 'MaW', 'RZ', 'RW')] <- c((1-rM)^2, (1-rM)^2, 1-((1-rM)^2), 1-((1-rM)^2))/2 # checked
+  tMatrix['ZW', 'MbMb', c('MbZ', 'MbW', 'RZ', 'RW')] <- c((1-rM)^2, (1-rM)^2, 1-((1-rM)^2), 1-((1-rM)^2))/2 # checked
+  tMatrix['ZW', 'MaMb', c('MaZ', 'MbZ', 'MaW', 'MbW', 'RZ', 'RW')] <- c((1 - rM), (1 - rM), (1 - rM), (1 - rM), 2*rM, 2*rM) / 4 # checked
+  tMatrix['ZW', 'RZ', c('ZZ', 'RZ', 'ZW', 'RW')] <- c(1, 1, 1, 1) / 4 # checked
+  tMatrix['ZW', 'MaR', c('MaZ', 'RZ', 'MaW', 'RW')] <- c(1-rM, (1-rM)+rM*2, 1-rM, (1-rM)+rM*2)/4 # checked
+  tMatrix['ZW', 'MbR', c('MbZ', 'RZ', 'MbW', 'RW')] <- c(1-rM, (1-rM)+rM*2, 1-rM, (1-rM)+rM*2)/4 # checked
+  tMatrix['ZW', 'RR', c('RZ', 'RW')] <- c(1, 1) / 2 # checked
   
-  tMatrix['MaW','ZZ', c('MaZ', 'ZW', 'RZ')] <- c(1-rM, 1, rM)/2
-  tMatrix['MaW','MaZ', c('MaW', 'MaMa', 'MaZ', 'ZW', 'RZ', 'RW', 'RR', 'MaR')] <- c(1-rM, (1-rM)^2, 1-rM, 1, rM, rM, rM^2, 2*(1-rM)*rM)/4
-  tMatrix['MaW','MbZ', c('MbW','MaMb','MaZ','ZW','RZ','RW','RR','MaR','MbR')] <- c((1 - rM), (1 - rM)^2, (1 - rM), 1, rM, rM, rM^2, rM*(1 - rM), rM*(1 - rM)) / 4
-  tMatrix['MaW','MaMa', c('MaMa', 'MaW', 'RR', 'RW', 'MaR')] <- c((1-rM)^2, (1-rM), rM^2, rM, 2*(1-rM)*rM)/2
-  tMatrix['MaW','MbMb', c('MaMb','MbW','RR','MaR','MbR','RW')] <- c((1 - rM)^2, (1 - rM), rM^2, rM*(1 - rM), rM*(1 - rM), rM) / 2
+  tMatrix['MaW','ZZ', c('MaZ', 'ZW', 'RZ')] <- c(1-rM, 1, rM)/2 # checked
+  tMatrix['MaW','MaZ', c('MaW', 'MaMa', 'MaZ', 'ZW', 'RZ', 'RW', 'RR', 'MaR')] <- c(1-rM, (1-rM)^2, 1-rM, 1, rM, rM, rM^2, 2*(1-rM)*rM)/4 # checked
+  tMatrix['MaW','MbZ', c('MbW','MaMb','MaZ','ZW','RZ','RW','RR','MaR','MbR')] <- c((1 - rM), (1 - rM)^2, (1 - rM), 1, rM, rM, rM^2, rM*(1 - rM), rM*(1 - rM)) / 4 # checked
+  tMatrix['MaW','MaMa', c('MaMa', 'MaW', 'RR', 'RW', 'MaR')] <- c((1-rM)^2, (1-rM), rM^2, rM, 2*(1-rM)*rM)/2 # checked
+  tMatrix['MaW','MbMb', c('MaMb','MbW','RR','MaR','MbR','RW')] <- c((1 - rM)^2, (1 - rM), rM^2, rM*(1 - rM), rM*(1 - rM), rM) / 2 # checked
   tMatrix['MaW','MaMb', c('MaMa', 'MaMb', 'MaW', 'MbW', 'RR', 'RW', 'MaR', 'MbR')] <- c((1 - rM)^2, (1 - rM)^2, (1 - rM), (1 - rM), 2*rM-(2*rM*(1-rM)), 2*rM, 3*rM*(1 - rM), rM*(1 - rM))/4 # checked
-  tMatrix['MaW','RZ', c('MaZ', 'MaR', 'ZW', 'RW', 'RZ', 'RR')] <- c((1-rM), (1-rM), 1, 1, rM, rM)/4
-  tMatrix['MaW','MaR', c('MaMa', 'MaR', 'MaW', 'RW', 'RR')] <- c((1-rM)^2, (1-rM)*(1+2*rM), (1-rM), (1+rM), rM*(1+rM))/4
-  tMatrix['MaW','MbR', c('MaR', 'MaMb', 'MbW', 'RW', 'RR', 'MbR')] <- c((1-rM)^2, (1-rM)*(1+2*rM), (1-rM), (1+rM), rM*(1+rM))/4
-  tMatrix['MaW','RR', c('MaR', 'RW', 'RR')] <- c((1-rM), 1, rM)/2
+  tMatrix['MaW','RZ', c('MaZ', 'MaR', 'ZW', 'RW', 'RZ', 'RR')] <- c((1-rM), (1-rM), 1, 1, rM, rM)/4 # checked
+  tMatrix['MaW','MaR', c('MaMa', 'MaR', 'MaW', 'RW', 'RR')] <- c((1-rM)^2, (1-rM)*(1+2*rM), (1-rM), (1+rM), rM*(1+rM))/4 # checked
+  tMatrix['MaW','MbR', c('MaR', 'MaMb', 'MbW', 'RW', 'RR', 'MbR')] <- c((1 - rM)*(1 + rM), (1 - rM)^2, (1 - rM), (1 + rM), rM*(1 + rM), rM*(1 - rM)) / 4 # checked
+  tMatrix['MaW','RR', c('MaR', 'RW', 'RR')] <- c((1-rM), 1, rM)/2 # checked
   
-  tMatrix['MbW','ZZ', c('MbZ', 'ZW', 'RZ')] <- c(1-rM, 1, rM)/2
-  tMatrix['MbW','MaZ', c('MaMb', 'MbZ', 'MaW', 'RZ', 'ZW', 'RW', 'RR', 'MaR', 'MbR')] <- c(1-rM, (1-rM)^2, 1-rM, 1, rM, rM, rM^2, 2*(1-rM)*rM)/4
-  tMatrix['MbW','MbZ', c('MbW', 'MbMb', 'MbZ', 'ZW', 'RZ', 'RW', 'RR', 'MbR')] <- c(1-rM, (1-rM)^2, 1-rM, 1, rM, rM, rM^2, 2*(1-rM)*rM)/4
-  tMatrix['MbW','MaMa', c('MaMb', 'MaW', 'RR', 'RW', 'MaR', 'MbR')] <- c((1-rM)^2, (1-rM), rM^2, rM, 2*(1-rM)*rM)/2
-  tMatrix['MbW','MbMb', c('MbMb', 'MbW', 'RR', 'RW', 'MbR')] <- c((1-rM)^2, (1-rM), rM^2, rM, 2*(1-rM)*rM)/2
-  tMatrix['MbW','MaMb', c('MaMb', 'MbMb', 'MaW', 'MbW', 'RW', 'MaR', 'MbR', 'RR')] <- c((1 - rM)^2, (1 - rM)^2, (1 - rM), (1 - rM), 2*rM, rM*(1 - rM), 3*rM*(1 - rM), 2*rM^2) / 4
-  tMatrix['MbW','RZ', c('MbZ', 'MbR', 'ZW', 'RW', 'RZ', 'RR')] <- c((1-rM), (1-rM), 1, 1, rM, rM)/4
-  tMatrix['MbW','MaR', c('MbR','MaR','RW','MaW','RR','MaMb')] <- c(rM*(1 - rM), (1 + rM)*(1 - rM), rM, (1 - rM), rM^2, (1 - rM)^2)/4
-  tMatrix['MbW','MbR', c('MbMb', 'MbR', 'MbW', 'RW', 'RR')] <- c((1-rM)^2, (1-rM)*(1+2*rM), (1-rM), (1+rM), rM*(1+rM))/4
-  tMatrix['MbW','RR', c('MR', 'RW', 'RR')] <- c((1-rM), 1, rM)/2
+  tMatrix['MbW','ZZ', c('MbZ', 'ZW', 'RZ')] <- c(1-rM, 1, rM)/2 # checked
+  tMatrix['MbW','MaZ', c('MbW','MaMb','MaZ','ZW','RZ','RW','RR','MaR','MbR')] <- c((1 - rM), (1 - rM)^2, (1 - rM), 1, rM, rM, rM^2, rM*(1 - rM), rM*(1 - rM)) / 4 # checked
+  tMatrix['MbW','MbZ', c('MbW', 'MbMb', 'MbZ', 'ZW', 'RZ', 'RW', 'RR', 'MbR')] <- c(1-rM, (1-rM)^2, 1-rM, 1, rM, rM, rM^2, 2*(1-rM)*rM)/4 # checked
+  tMatrix['MbW','MaMa', c('MaMb', 'MaW', 'RR', 'MaR', 'MbR', 'RW')] <- c((1 - rM)^2, (1 - rM), rM^2, rM*(1 - rM), rM*(1 - rM), rM) / 2 # checked
+  tMatrix['MbW','MbMb', c('MbMb', 'MbW', 'RR', 'RW', 'MbR')] <- c((1-rM)^2, (1-rM), rM^2, rM, 2*(1-rM)*rM)/2 # checked
+  tMatrix['MbW','MaMb', c('MbMb', 'MaMb', 'MaW', 'MbW', 'RR', 'RW', 'MaR', 'MbR')] <- c((1 - rM)^2, (1 - rM)^2, (1 - rM), (1 - rM), 2*rM-(2*rM*(1-rM)), 2*rM, 3*rM*(1 - rM), rM*(1 - rM))/4 # checked
+  tMatrix['MbW','RZ', c('MbZ', 'MbR', 'ZW', 'RW', 'RZ', 'RR')] <- c((1-rM), (1-rM), 1, 1, rM, rM)/4 # checked
+  tMatrix['MbW','MaR', c('MbR', 'MaMb', 'MaW', 'RW', 'RR', 'MaR')] <- c((1 - rM)*(1 + rM), (1 - rM)^2, (1 - rM), (1 + rM), rM*(1 + rM), rM*(1 - rM)) / 4 # checked
+  tMatrix['MbW','MbR', c('MbMb', 'MbR', 'MbW', 'RW', 'RR')] <- c((1-rM)^2, (1-rM)*(1+2*rM), (1-rM), (1+rM), rM*(1+rM))/4 # checked
+  tMatrix['MbW','RR', c('MR', 'RW', 'RR')] <- c((1-rM), 1, rM)/2 # checked
   
-  tMatrix['RW','ZZ', c('RZ', 'ZW')] <- c(1, 1)/2
-  tMatrix['RW','MaZ', c('MaR', 'RZ', 'MaW', 'ZW', 'RR', 'RW')] <- c((1-rM), 1, (1-rM), 1, rM, rM)/4
-  tMatrix['RW','MbZ', c('MbR', 'RZ', 'MbW', 'ZW', 'RR', 'RW')] <- c((1-rM), 1, (1-rM), 1, rM, rM)/4
-  tMatrix['RW','MaMa', c('MaR', 'MaW', 'RR', 'RW')] <- c((1-rM), (1-rM), rM, rM)/2
-  tMatrix['RW','MbMb', c('MbR', 'MbW', 'RR', 'RW')] <- c((1-rM), (1-rM), rM, rM)/2
-  tMatrix['RW','MaMb', c('MaR', 'MbR', 'MaW', 'MbW', 'RR', 'RW')] <- c((1 - rM), (1 - rM), (1 - rM), (1 - rM), 2*rM, 2*rM) / 4
-  tMatrix['RW','RZ', c('RR', 'RZ', 'RW', 'ZW')] <- c(1, 1, 1, 1)/4
-  tMatrix['RW','MaR', c('RW', 'MaR', 'RR', 'MaW')] <- c((1+rM), (1-rM), (1+rM), (1-rM))/4
-  tMatrix['RW','MbR', c('RW', 'MbR', 'RR', 'MbW')] <- c((1+rM), (1-rM), (1+rM), (1-rM))/4
-  tMatrix['RW','RR', c('RW', 'RR')] <- c(1, 1)/2
+  tMatrix['RW','ZZ', c('RZ', 'ZW')] <- c(1, 1)/2 # checked
+  tMatrix['RW','MaZ', c('MaR', 'RZ', 'MaW', 'ZW', 'RR', 'RW')] <- c((1-rM), 1, (1-rM), 1, rM, rM)/4 # checked
+  tMatrix['RW','MbZ', c('MbR', 'RZ', 'MbW', 'ZW', 'RR', 'RW')] <- c((1-rM), 1, (1-rM), 1, rM, rM)/4 # checked
+  tMatrix['RW','MaMa', c('MaR', 'MaW', 'RR', 'RW')] <- c((1-rM), (1-rM), rM, rM)/2 # checked
+  tMatrix['RW','MbMb', c('MbR', 'MbW', 'RR', 'RW')] <- c((1-rM), (1-rM), rM, rM)/2 # checked
+  tMatrix['RW', 'MaMb', c('MaR', 'MbR', 'RR', 'MaW', 'MbW', 'RW')] <- c((1 - rM), (1 - rM), 2*rM, (1 - rM), (1 - rM), 2*rM) / 4 # checked
+  tMatrix['RW','RZ', c('RR', 'RZ', 'RW', 'ZW')] <- c(1, 1, 1, 1)/4 # checked
+  tMatrix['RW','MaR', c('RW', 'MaR', 'RR', 'MaW')] <- c((1+rM), (1-rM), (1+rM), (1-rM))/4 # checked
+  tMatrix['RW','MbR', c('RW', 'MbR', 'RR', 'MbW')] <- c((1+rM), (1-rM), (1+rM), (1-rM))/4 # checked
+  tMatrix['RW','RR', c('RW', 'RR')] <- c(1, 1)/2 # checked
   
   ## Define viability mask
   viabilityMask <- array(data = 1, dim = c(size, size, size), dimnames = list(gtype, gtype, gtype))
